deab61301c0f89cbff7d67fb308a4e2fa7fec2db
bsullivan19 <sullivan.bre@northeastern.edu>
2023-03-30 18:37:13 -0400

    Added timeAsked to Question and OfficeHoursQuestion model. Added comments.

--- shared/types/CoveyTownSocket.d.ts
+++ shared/types/CoveyTownSocket.d.ts
@@ -83,6 +83,7 @@ export interface OfficeHoursQuestion {
   questionContent: string;
   students: string[];
   groupQuestion: boolean;
+  timeAsked?: number;
 }
 
 export interface OfficeHoursQueue {
